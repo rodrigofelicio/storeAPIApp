aa41a09ffb15631df0da804f38da1831eaeaf97d
rodrigofelicio <rodrigo.felicio@gmail.com>
2024-06-03 15:05:29 -0300

    stores and items DB; requirements.txt; .flaskenv; steps.txt; API endpoints for stores and items;

---
+++ db.py
@@ -0,0 +1,11 @@
+stores = {}
+items = {
+    1: {
+            "name": "Chair",
+            "price": 15.99        
+    },
+    2: {
+            "name": "Table",
+            "price": 180.88
+    }
+}
